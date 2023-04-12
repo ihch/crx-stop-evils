9f2ae780323f6858410bc66522677e2321f167a5
ihch <nemu0sou@gmail.com>
2023-04-12 18:07:10 +0900

    feat: implemented base features

---
+++ scripts/content.js
@@ -0,0 +1,19 @@
+console.log('Stop watching YouTube or other evils');
+
+const bodyElement = document.querySelector('body');
+
+const isTimeWatchableVideo = (hours) => {
+    return hours >= 19;
+};
+
+const coverContents = (hours) => {
+    if (isTimeWatchableVideo(hours)) {
+        return;
+    }
+    bodyElement.textContent = 'lol';
+};
+
+if (bodyElement) {
+    const currentTime = new Date();
+    coverContents(currentTime.getHours());
+}
